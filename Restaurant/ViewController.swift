d2da50701c7bbbff775ff3004709e27b177214aa
Sam Kortekaas <samkortekaas1@gmail.com>
2017-11-28 10:26:35 +0100

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Sam Kortekaas on 28/11/2017.
+//  Copyright © 2017 Kortekaas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
